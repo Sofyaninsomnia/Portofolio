6c59704eed7521ce777a39bf4b50b5362ca3b8cb
Rizky-ai-bit <rizkygamimg17@gmail.com>
2025-07-24 11:52:49 +0700

    Kamis

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const sidebarMenu = document.getElementById('sidebarMenu');
-  const menuIcon = document.getElementById('menuIcon');
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebarMenu = document.getElementById("sidebarMenu");
+  const menuIcon = document.getElementById("menuIcon");
 
   if (sidebarMenu && menuIcon) {
-    sidebarMenu.addEventListener('show.bs.offcanvas', function () {
-      menuIcon.classList.remove('bi-list');
-      menuIcon.classList.add('bi-x'); 
+    sidebarMenu.addEventListener("show.bs.offcanvas", function () {
+      menuIcon.classList.remove("bi-list");
+      menuIcon.classList.add("bi-x");
     });
 
-       sidebarMenu.addEventListener('hide.bs.offcanvas', function () {
-      menuIcon.classList.remove('bi-x'); 
-      menuIcon.classList.add('bi-list');   
+    sidebarMenu.addEventListener("hide.bs.offcanvas", function () {
+      menuIcon.classList.remove("bi-x");
+      menuIcon.classList.add("bi-list");
     });
   }
 });
+
