fde222ca8ec4879f256e30a7fa8be1307059aa68
Rizky-ai-bit <rizkygamimg17@gmail.com>
2025-07-24 10:49:45 +0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebarMenu = document.getElementById('sidebarMenu');
+  const menuIcon = document.getElementById('menuIcon');
+
+  if (sidebarMenu && menuIcon) {
+    sidebarMenu.addEventListener('show.bs.offcanvas', function () {
+      menuIcon.classList.remove('bi-list');
+      menuIcon.classList.add('bi-x'); 
+    });
+
+       sidebarMenu.addEventListener('hide.bs.offcanvas', function () {
+      menuIcon.classList.remove('bi-x'); 
+      menuIcon.classList.add('bi-list');   
+    });
+  }
+});
